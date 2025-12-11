622d147314b35eb2f692cf165aa5643a84990cce
Mifta Widaya <miftawidaya@gmail.com>
2025-12-11 09:03:06 +0700

    fix: remove unused path import

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import path from 'path';
+
 import swaggerUi from 'swagger-ui-express';
 import swaggerJsdoc from 'swagger-jsdoc';
 import { todosRouter } from './routes/todos';
